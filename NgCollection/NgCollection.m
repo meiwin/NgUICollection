550a57689c9249cc725cad929608fb3e4e961072
meiwin.fu <meiwin.fu@bubblemotion.com>
2015-03-14 22:54:54 +0800

    Initial Commit

---
+++ NgCollection/NgCollection.m
@@ -0,0 +1,13 @@
+//
+//  NgCollection.m
+//  NgCollection
+//
+//  Created by Meiwin Fu on 14/3/15.
+//  Copyright (c) 2015 Meiwin. All rights reserved.
+//
+
+#import "NgCollection.h"
+
+@implementation NgCollection
+
+@end
